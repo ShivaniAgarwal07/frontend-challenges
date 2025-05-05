31c050fb3026a0d698deddd494c10ec49598d65d
Shivani Agarwal <shivaniagar7@gmail.com>
2025-05-05 18:01:41 +0530

    day07 array flattening-resursion method

--- src/day07/ArrayFlatten.jsx
+++ src/day07/ArrayFlatten.jsx
@@ -1,7 +1,23 @@
 function flattenArray(arr) {
     // Your implementation
-    const flatArr = arr.flat(Infinity);
-    return flatArr
+    // const flatArr = arr.flat(Infinity);
+    // return flatArr
+
+    const result = [];
+    for (let i =0; i<arr.length; i++){
+        let elem= arr[i];
+
+        if(Array.isArray(elem)){
+            result.push(...flattenArray(elem))
+        }else{
+            result.push(elem)
+        }
+    }
+
+    return result
+
+
+
 }
 
 console.log(flattenArray([['a', 'a'], ['e','o','p'], ['e','l',['u']]]));
